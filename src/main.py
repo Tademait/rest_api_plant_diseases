f8c50c451757bedd50973f53e42c1c91a14d4801
Tadeas Kozub <tkozub@redhat.com>
2023-03-13 00:25:54 +0100

    added an image resize functionality and a temporary dummy class_labels list so that the api response is finally realistic

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from tensorflow import keras
 from pydantic import BaseModel
 import io
 import numpy as np
+import json
 
 class Analyzer:
     def __init__(self) -> None:
@@ -23,14 +24,9 @@ class PredictionModel:
         self.model = None
 
     def load_model(self, path_to_model: str):
-        # Load model
-        # Define custom objects for loading the model
         self.model = keras.models.load_model(path_to_model)
-        # self.model.compile(loss="sparse_categorical_crossentropy",
-        #        optimizer="nadam",
-        #        metrics=["accuracy"])
-        print("Succsefully loaded model")
-        print(self.model.summary())
+        print(f"Succsefully loaded model from {path_to_model}")
+        #print(self.model.summary())
 
     @staticmethod
     def prepare_data(data):
@@ -41,11 +37,12 @@ class PredictionModel:
         
         
     def predict(self, data):
-        # Predict
         if not self.model:
             raise NotImplementedError #todo implement own Error class
         # prepared_data = self.prepare_data()
         return self.model.predict(data)
+    
+    
 app = FastAPI()
 def prepare_analyzer():
     return Analyzer()
@@ -55,16 +52,12 @@ async def root(analyzer_obj: Analyzer = Depends(prepare_analyzer)):
     analyzer_obj.load_all_models()
     return {"message": "Hello World"}
 
-@app.post("/identify")
-async def predict():
-    # print(type(identif.image))
-    return {"list of predictions"}
 
 @app.post("/api/v1/uploadfile")
 async def create_upload_file(image: UploadFile = File(...), plant: str = Form(...)):
     contents = await image.read()
     img = Image.open(io.BytesIO(contents))
-    # img.show()
+    img = img.resize((256, 256))
     
     # Convert image to numpy array
     np_array = np.array(img)
@@ -75,13 +68,19 @@ async def create_upload_file(image: UploadFile = File(...), plant: str = Form(..
     np_array = np_array / 255.0
     # add extra dimension for batch size
     input_batch = np.expand_dims(np_array, axis=0) 
-    print(tomato_model.model.input_shape)
-    print(input_batch)
-    tomato_model.model.compile()
+    
     predictions = tomato_model.predict(input_batch)
-    print(" | Prediction list: |")
+    np.set_printoptions(formatter={'float_kind':'{:f}'.format})
     print(predictions)
-    return {"image received successfully": image.filename, "yes" : plant}
+    
+    #! todo: make this more generic, either save the labels as part of model, or inside the folder
+    class_labels = ['tomato_bacterial_spot', 'tomato_early_blight', 'tomato_late_blight',
+                    'tomato_leaf_mold', 'tomato_septoria_leaf_spot', 'tomato_spider_mites',
+                    'tomato_target_spot', 'tomato_yellow_leaf_curl_virus', 'tomato_mosaic_virus', 'tomato_healthy']
+
+
+    api_data = {class_labels[i]: float(predictions[0][i]) for i in range(len(class_labels))}
+    return api_data
     
 if __name__ == "__main__":
     import uvicorn
@@ -89,7 +88,7 @@ if __name__ == "__main__":
     # tomato_model = PredictionModel()
     # tomato_model.load_model("tomato_model/v1/")
     # plant_models = {"tomato": tomato_model}
-    analyzer_obj = prepare_analyzer()
+    #analyzer_obj = prepare_analyzer()
     
     # when calling the identify endpoint:
     # + on startup, load-up every plant model - either based on loop through
