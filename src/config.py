e4ac42cceb4be2ad9946c4cd30bbdf4c134ad64d
Tadeas Kozub <tkozub@redhat.com>
2023-03-19 16:43:42 +0100

    changed labels to have the same form as in the DB

--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
 MODELS = {"tomato": "tomato_model/v1/"}
 
 # proper labels for each model
-_TOMATO_LABELS = ['tomato_bacterial_spot', 'tomato_early_blight', 'tomato_late_blight',
-                  'tomato_leaf_mold', 'tomato_septoria_leaf_spot', 'tomato_spider_mites',
-                  'tomato_target_spot', 'tomato_yellow_leaf_curl_virus', 'tomato_mosaic_virus', 'tomato_healthy']
+_TOMATO_LABELS = ['bacterial spot', 'early blight', 'late blight',
+                  'leaf mold', 'septoria leaf spot', 'spider mites',
+                  'target spot', 'yellow leaf curl virus', 'mosaic virus', 'healthy']
 
 LABELS = {"tomato": _TOMATO_LABELS}
