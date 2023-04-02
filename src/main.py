644208b0709d5421f291706f034226b304166467
Tadeas Kozub <tkozub@redhat.com>
2023-04-02 18:07:52 +0200

    add second image analysis and proper weight adjust of concatted results

--- src/main.py
+++ src/main.py
@@ -60,38 +60,72 @@ async def test_db(db: database.Database = Depends(get_db)):
 
 
 @app.post("/api/v1/uploadfile")
-async def create_upload_file(analyzer: Analyzer = Depends(get_analyzer), image1: UploadFile = File(...), image2: UploadFile = File(...), plant: str = Form(...)):
+async def analyze_images(analyzer: Analyzer = Depends(get_analyzer), image1: UploadFile = File(...), image2: UploadFile = File(...), plant: str = Form(...)):
     if plant not in analyzer.models.keys():
-        raise HTTPException(status_code=404, detail="No model available for the provided plant")
+        raise HTTPException(status_code=500, detail="No model available for the provided plant")
+    
+    # select proper model for the requested plant and get the input dimensions to fit model's input layer
+    pred_model = analyzer.models[plant]
+    dimensions = pred_model.model.layers[0].input_shape[1:3]
+    print(dimensions)
+    
+    if type(dimensions) is not tuple:
+        raise HTTPException(status_code=500, detail="Model dimensions are not supported")
+    if len(dimensions) != 2:
+        raise HTTPException(status_code=500, detail="Model dimensions are not supported")
     
     contents1 = await image1.read()
     img1 = Image.open(io.BytesIO(contents1))
-    img1 = img1.resize((256, 256))
-    
     contents2 = await image2.read()
     img2 = Image.open(io.BytesIO(contents2))
-    img2 = img2.resize((256, 256))
-
+    
     # collect the image to enlarge dataset
     if config.COLLECTION_ENABLED:
-        file_name = datetime.now().strftime('%Y_%m_%d-%I_%M_%S')
-        img1.save(f"{config.COLLECTION_PATH}/{file_name}-1.jpg")
-        img2.save(f"{config.COLLECTION_PATH}/{file_name}-2.jpg")
+        try:
+            file_name = datetime.now().strftime('%Y_%m_%d-%I_%M_%S')
+            img1.save(f"{config.COLLECTION_PATH}/{file_name}-1.jpg")
+            img2.save(f"{config.COLLECTION_PATH}/{file_name}-2.jpg")
+        except:
+            print("Failed to save images")
 
+    # resize the image to input dimensions of the model
+    img1 = img1.resize(dimensions)
+    img2 = img2.resize(dimensions)
+    
+    # Convert images to RGB mode to remove the alpha channel if present
+    # for example: user sends a PNG image with transparency
+    img1 = img1.convert('RGB')
+    img2 = img2.convert('RGB')
+    
     # Convert image to numpy array
-    np_array = np.array(img1)
-    pred_model = analyzer.models[plant]
+    np_array1 = np.array(img1)
+    np_array2 = np.array(img2)
 
     # normalize the values
     # np_array = np_array / 255.0
     
-    # add extra dimension for batch size
-    input_batch = np.expand_dims(np_array, axis=0) 
+    # add extra dimension to create a batch of size 1
+    input_batch1 = np.expand_dims(np_array1, axis=0)
+    input_batch2 = np.expand_dims(np_array2, axis=0)
 
-    predictions = pred_model.predict(input_batch)
-    predictions = [{"name": pred_model.labels[i], "percentage": float(predictions[0][i])} for i in range(len(pred_model.labels))]
-    predictions.sort(key=lambda x: x["percentage"], reverse=True)
-    top_predictions = predictions[:5]
+    # gather predictions from both models
+    predictions1 = pred_model.predict(input_batch1)
+    predictions2 = pred_model.predict(input_batch2)
+    
+    # concat the results and find max value for each pair
+    predictions_concat = np.concatenate((predictions1.reshape(-1, 1), predictions2.reshape(-1, 1)), axis=1)
+    predictions = np.amax(predictions_concat, axis=1)
+    
+    # use softmax function on the concatenated predictions
+    # predictions = np.exp(predictions) / np.sum(np.exp(predictions), axis=0)
+
+    # adjust the weights of the predictions to add up to 1 (100%)
+    predictions = predictions / np.sum(predictions)
+
+    # create a list of dictionaries with the results and return top 5 predictions
+    predictions_array = [{"name": pred_model.labels[i], "percentage": float(predictions[i])} for i in range(len(pred_model.labels))]
+    predictions_array.sort(key=lambda x: x["percentage"], reverse=True)
+    top_predictions = predictions_array[:5]
     return top_predictions
 
 
