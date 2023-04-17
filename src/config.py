de02089a511affb02ee9ef1ca751800a94f045e9
Tadeas Kozub <tkozub@redhat.com>
2023-04-17 15:15:05 +0200

    adjusted config settings

--- src/config.py
+++ src/config.py
@@ -6,14 +6,16 @@ load_dotenv()
 DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
 
 # points plant to according model
-MODELS = {"tomato": "/var/data/tomato_model/v3/"}
+MODELS = {"tomato": "tomato_model/v1/", "grape": "grape_model/v1/"}
 
 # proper labels for each model
 _TOMATO_LABELS = ['bacterial spot', 'early blight', 'late blight',
                   'leaf mold', 'septoria leaf spot', 'spider mites',
                   'target spot', 'yellow leaf curl virus', 'mosaic virus', 'healthy']
 
-LABELS = {"tomato": _TOMATO_LABELS}
+_GRAPE_LABELS = ['black rot', 'esca', 'leaf blight', 'healthy']
+
+LABELS = {"tomato": _TOMATO_LABELS, "grape": _GRAPE_LABELS}
 
 # image collection configuration
 COLLECTION_ENABLED = True
